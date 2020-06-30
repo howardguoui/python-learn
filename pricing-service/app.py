1fd029f0567db1366f7c919d19d2bac581505d5b
Howard <howardguoui@gmail.com>
2020-06-29 20:52:43 -0400

    mongo
    mongo

--- pricing-service/app.py
+++ pricing-service/app.py
@@ -1,22 +1,40 @@
-from typing import Dict
-from bs4 import BeautifulSoup
-import requests
-import re
+# from typing import Dict
+# from bs4 import BeautifulSoup
+# import requests
+# import re
+#
+# URL = "https://www.johnlewis.com/2020-apple-ipad-pro-12-9-inch-a12z-bionic-ios-wi-fi-256gb/space-grey/p4949087"
+# TAG_NAME = "p"
+# QUERY = {"class": "price price--large"}
+#
+# request = requests.get(URL)
+# content = request.content
+# soup = BeautifulSoup(content, "html.parser")
+# element = soup.find(TAG_NAME, QUERY)
+# string_price = element.text.strip()
+#
+# pattern = re.compile(r"(\d+,?\d+\.\d+)")
+# match = pattern.search(string_price)
+# found_price = match.group(1)
+# without_commas = found_price.replace(",", "")
+# price = float(without_commas)
+#
+# print(price)
+from models.item import Item
 
-URL = "https://www.johnlewis.com/2020-apple-ipad-pro-12-9-inch-a12z-bionic-ios-wi-fi-256gb/space-grey/p4949087"
-TAG_NAME = "p"
-QUERY = {"class": "price price--large"}
+ipad = Item(
+    "https://www.johnlewis.com/2020-apple-ipad-pro-12-9-inch-a12z-bionic-ios-wi-fi-256gb/space-grey/p4949087",
+    "p",
+    {"class": "price price--large"}
+)
 
-request = requests.get(URL)
-content = request.content
-soup = BeautifulSoup(content, "html.parser")
-element = soup.find(TAG_NAME, QUERY)
-string_price = element.text.strip()
+ipad.save_to_mongo()
 
-pattern = re.compile(r"(\d+,?\d+\.\d+)")
-match = pattern.search(string_price)
-found_price = match.group(1)
-without_commas = found_price.replace(",", "")
-price = float(without_commas)
+# Check the item is in MongoDB
+#   Use correct database
+#   Find colleciton
+#   Check item
 
-print(price)
+items_loaded = Item.all()
+print(items_loaded)
+print(items_loaded[0].load_price())
--- pricing-service/models/item.py
+++ pricing-service/models/item.py
@@ -1,45 +1,50 @@
-from dataclasses import dataclass, field
-from typing import Dict
-import uuid
+from typing import Dict, List
 from bs4 import BeautifulSoup
 import requests
 import re
-from models.model import Model
+import uuid
 from common.database import Database
-from models.store import Store
 
 
-@dataclass(eq=False)
-class Item(Model):
-    collection: str = field(init=False, default="items")
-    name: str
-    url: str
-    price: float = None
-    _id: str = field(default_factory=lambda: uuid.uuid4().hex)
-    tag_name: str = field(init=False)
-    query: Dict = field(init=False)
+class Item:
+    def __init__(self, url: str, tag_name: str, query: Dict, _id: str = None):
+        super().__init__()
+        self.url = url
+        self.tag_name = tag_name
+        self.query = query
+        self.price = None
+        self.collection = "items"
+        self._id = _id or uuid.uuid4().hex
 
-    def __post_init__(self):
-        store = Store.find_by_url(self.url)
-        self.tag_name = store.tag_name
-        self.query = store.query
+    def __repr__(self):
+        return f"<Item {self.url}>"
 
-    def load_price(self):
+    def load_price(self) -> float:
         request = requests.get(self.url)
         content = request.content
         soup = BeautifulSoup(content, "html.parser")
         element = soup.find(self.tag_name, self.query)
         string_price = element.text.strip()
 
-        pattern = re.compile(r"(\d+.\d+)")
+        pattern = re.compile(r"(\d+,?\d+\.\d+)")
         match = pattern.search(string_price)
-        self.price = float(match.group(1))
+        found_price = match.group(1)
+        without_commas = found_price.replace(",", "")
+        self.price = float(without_commas)
         return self.price
 
-    def json(self):
+    def json(self) -> Dict:
         return {
             "_id": self._id,
-            "name": self.name,
             "url": self.url,
-            "price": self.price,
+            "tag_name": self.tag_name,
+            "query": self.query
         }
+
+    @classmethod
+    def all(cls) -> List["Item"]:
+        items_from_db = Database.find("items", {})
+        return [cls(**item) for item in items_from_db]
+
+    def save_to_mongo(self) -> None:
+        Database.insert(self.collection, self.json())
